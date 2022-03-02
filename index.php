9971d51476e829fbb79d4c55d414a3566006b3c4
ringkurahman <mrringkurahman@gmail.com>
2022-03-03 03:29:05 +0600

    Basic Bootstrapping Finish

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Vocabulary Builder</title>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+    <link rel="stylesheet" href="//cdn.rawgit.com/necolas/normalize.css/master/normalize.css">
+    <link rel="stylesheet" href="//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css">
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+          integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body class="home">
+<div class="container" id="main">
+    <div class="row">
+            <h1 class="maintitle"><i class="fas fa-language"></i>My Vocabularies</h1>
+    </div>
+    <div class="row navigation">
+    	<div class="column column-60 column-offset-20">
+    		<div class="formaction">
+    			<a href="#" id="login">Login</a> | <a href="#" id="register">Register Account</a>
+    		</div>
+    		<div class="formc">
+    			<form id="form01" action="tasks.php" method="POST">
+    				<h3>Register</h3>
+    				<fieldset>
+    					<label for="email">Email</label>
+    					<input type="text" name="email" id="email" placeholder="Email Address">
+    					<label for="password">Password</label>
+    					<input type="password" name="password" id="password" placeholder="Password">
+    					<input class="button-primary" type="submit" value="submit">
+    					<input type="hiden" name="action" value="register">
+    				</fieldset>
+    			</form>
+    		</div>
+    	</div>
+    </div>
+</div>
+</body>
+</html>
